e42f5374025fd80c8bd8324ef698fbeab13f9db7
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-01 11:22:26 +0700

    1234

---
+++ core_workshop/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default () => {
+  return (
+    <>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </>
+  );
+}
