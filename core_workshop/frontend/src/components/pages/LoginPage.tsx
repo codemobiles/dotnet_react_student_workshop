2f5ac73d9af67d870b33fdf3b3e1b8ec9ac46594
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-02 10:02:58 +0700

    1234

--- core_workshop/frontend/src/components/pages/LoginPage.tsx
+++ core_workshop/frontend/src/components/pages/LoginPage.tsx
@@ -5,47 +5,17 @@ type Props = {};
 
 export default function LoginPage({}: Props) {
   const defaultAccount = { username: "", password: "" };
-  // let account = defaultAccount;
-  const [account, setAccount] = useState(defaultAccount);
+
   return (
     <>
       <form>
         <Stack direction="column" spacing={1}>
-          <TextField
-            id="outlined-basic"
-            label="Username"
-            variant="outlined"
-            value={account.username}
-            onChange={(e) => {
-              setAccount({
-                username: e.target.value,
-                password: account.password,
-              });
-            }}
-          />
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            value={account.password}
-            onChange={(e) => {
-              setAccount({
-                username: account.username,
-                password: e.target.value,
-              });
-            }}
-          />
+          <TextField id="outlined-basic" label="Username" variant="outlined" />
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
 
           <Box style={{ height: 20 }} />
-          <Button
-            variant="contained"
-            onClick={() => alert(JSON.stringify(account))}
-          >
-            Login
-          </Button>
-          <Button variant="outlined" onClick={() => setAccount(defaultAccount)}>
-            Clear
-          </Button>
+          <Button variant="contained">Login</Button>
+          <Button variant="outlined">Clear</Button>
         </Stack>
       </form>
     </>
