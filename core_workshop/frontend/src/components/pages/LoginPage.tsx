2c0b36dd8bc4d32b15f7d6c4b29cd55230432a55
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-02 09:18:04 +0700

    1234

--- core_workshop/frontend/src/components/pages/LoginPage.tsx
+++ core_workshop/frontend/src/components/pages/LoginPage.tsx
@@ -1,3 +1,4 @@
+import { Stack, TextField } from "@mui/material";
 import React from "react";
 
 type Props = {};
@@ -6,7 +7,12 @@ export default function LoginPage({}: Props) {
   // https://docs.google.com/document/d/1Jd-sGeZMvMm0H1QY395Z8Fz5VzPzS9jdktkjbUQLuBA/edit?usp=sharing
   return (
     <>
-      <form></form>
+      <form>
+        <Stack direction="column" spacing={3}>
+          <TextField id="outlined-basic" label="Username" variant="outlined" />
+          <TextField id="outlined-basic" label="Username" variant="outlined" />
+        </Stack>
+      </form>
     </>
   );
 }
