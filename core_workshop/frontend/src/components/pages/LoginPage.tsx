da22f2fe533b903de8bb1e0fdf8fdc67b942b1c6
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-02 10:47:49 +0700

    1234

--- core_workshop/frontend/src/components/pages/LoginPage.tsx
+++ core_workshop/frontend/src/components/pages/LoginPage.tsx
@@ -1,77 +1,152 @@
-import { Box, Button, Stack, TextField } from "@mui/material";
-import React, { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+// import loginBg from "../assets/images/bg4.jpg";
+import { User } from "../../types/user.type";
 import { yupResolver } from "@hookform/resolvers/yup";
+import * as Icons from "@mui/icons-material/";
+import { Box, InputAdornment } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { Controller, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 
 const formValidateSchema = Yup.object().shape({
+  // username: Yup.string().email("Invalid email address").required("Email is required").trim(),
   username: Yup.string()
-    .email("Username must be email format")
-    .required("Username is required")
+    .min(4)
+    .required("Username must be more than 3 letters")
     .trim(),
   password: Yup.string().required("Password is required").trim(),
 });
 
-type Props = {};
+const Login = () => {
+  const navigate = useNavigate();
 
-export default function LoginPage({}: Props) {
-  const defaultAccount = { username: "", password: "" };
+  const classes: any = {
+    root: { display: "flex", justifyContent: "center", alignItems: "center" },
+    submitBtn: { marginTop: 4 },
+    canelBtn: { marginTop: 2 },
+  };
+
+  const initialValue: User = { username: "admin", password: "12341234" };
   const {
     control,
     handleSubmit,
-    reset,
     formState: { errors },
-  } = useForm({
-    defaultValues: defaultAccount,
+  } = useForm<User>({
+    defaultValues: initialValue,
     resolver: yupResolver(formValidateSchema),
   });
 
-  return (
-    <>
-      <form
-        onSubmit={handleSubmit((value) => {
-          alert(JSON.stringify(value));
-        })}
-      >
-        <Stack direction="column" spacing={1}>
-          {/* Username */}
-          <Controller
-            name="username"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                id="outlined-basic"
-                label="Username"
-                variant="outlined"
-                error
-                helperText="Username is required"
-                {...field}
-              />
-            )}
-          />
+  const onSubmit = async (values: User) => {};
 
-          {/* Password */}
-          <Controller
-            name="password"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                id="outlined-basic"
-                label="Password"
-                variant="outlined"
-                {...field}
-              />
-            )}
-          />
-          <Box style={{ height: 20 }} />
-          <Button type="submit" variant="contained">
-            Login
-          </Button>
-          <Button variant="outlined" onClick={() => reset(defaultAccount)}>
-            Clear
-          </Button>
-        </Stack>
+  const showForm = () => {
+    return (
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="username"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              error={Boolean(errors.username?.message)}
+              helperText={errors.username?.message}
+              {...field}
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              label="Username"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icons.Email />
+                  </InputAdornment>
+                ),
+              }}
+              autoComplete="email"
+              autoFocus
+            />
+          )}
+        ></Controller>
+
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              helperText={errors.password?.message}
+              error={Boolean(errors.password?.message)}
+              {...field}
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icons.Password />
+                  </InputAdornment>
+                ),
+              }}
+              label="Password"
+              type="password"
+            />
+          )}
+        ></Controller>
+
+        {/* {authReducer.isError && <Alert severity="error">Login failed</Alert>} */}
+
+        <Button
+          sx={classes.submitBtn}
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+        >
+          Login
+        </Button>
+
+        <Button
+          onClick={() => {
+            navigate("/register");
+          }}
+          type="button"
+          fullWidth
+          variant="outlined"
+          className="border-dashed border-1 border-gray-300 mt-4"
+          color="primary"
+        >
+          Register
+        </Button>
       </form>
-    </>
+    );
+  };
+
+  return (
+    <Box className="flex justify-center items-center">
+      <Card className="max-w-[345px]">
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            Login
+          </Typography>
+          {showForm()}
+        </CardContent>
+      </Card>
+      {/* <style>
+        {`
+          body {
+            min-height: 100vh;
+            position: relative;
+            margin: 0;
+            background-size: cover;
+            background-image: url(${loginBg});
+            text-align: center;
+          }
+        `}
+      </style> */}
+    </Box>
   );
-}
+};
+
+export default Login;
