573c29e7e7d8a7f7bd352fcd6f0533a242c574a9
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-02 09:30:17 +0700

    1234

--- core_workshop/frontend/src/components/pages/LoginPage.tsx
+++ core_workshop/frontend/src/components/pages/LoginPage.tsx
@@ -1,16 +1,35 @@
-import { Stack, TextField } from "@mui/material";
+import { Box, Button, Stack, TextField } from "@mui/material";
 import React from "react";
 
 type Props = {};
 
 export default function LoginPage({}: Props) {
-  // https://docs.google.com/document/d/1Jd-sGeZMvMm0H1QY395Z8Fz5VzPzS9jdktkjbUQLuBA/edit?usp=sharing
+  let account = { username: "", password: "" };
   return (
     <>
       <form>
-        <Stack direction="column" spacing={3}>
-          <TextField id="outlined-basic" label="Username" variant="outlined" />
-          <TextField id="outlined-basic" label="Username" variant="outlined" />
+        <Stack direction="column" spacing={1}>
+          <TextField
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+            onChange={(e) => (account.username = e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            onChange={(e) => (account.password = e.target.value)}
+          />
+
+          <Box style={{ height: 20 }} />
+          <Button
+            variant="contained"
+            onClick={() => alert(JSON.stringify(account))}
+          >
+            Login
+          </Button>
+          <Button variant="outlined">Clear</Button>
         </Stack>
       </form>
     </>
