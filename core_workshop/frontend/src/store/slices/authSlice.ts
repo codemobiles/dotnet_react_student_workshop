561a35fa9b081375ee4d1ed2bf936dfb7d507341
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-03 14:50:18 +0700

    1234

--- core_workshop/frontend/src/store/slices/authSlice.ts
+++ core_workshop/frontend/src/store/slices/authSlice.ts
@@ -1,8 +1,23 @@
+import { LoginResult, RegisterResult } from "@/types/auth-result.type";
 import { createSlice } from "@reduxjs/toolkit";
+
+export interface AuthState {
+  loginResult?: LoginResult;
+  registerResult?: RegisterResult;
+  isAuthenticating: boolean;
+  isAuthented: boolean;
+  isError: boolean;
+}
+
+const initialState: AuthState = {
+  isAuthenticating: true,
+  isAuthented: false,
+  isError: false,
+};
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: {},
+  initialState,
   reducers: {},
 });
 
