cfdea528f3ced3b501fe4c9674b231712957f4c3
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-03 09:59:46 +0700

    1234

--- core_workshop/backend/Services/AuthRepository.cs
+++ core_workshop/backend/Services/AuthRepository.cs
@@ -31,9 +31,52 @@ namespace backend.Services
 
         public void Register(User user)
         {
+            user.Password = CreatePasswordHash(user.Password!);
+            _context.Add(user);
+            _context.SaveChanges();
+        }
 
+        private string CreatePasswordHash(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+            return $"{Convert.ToBase64String(salt)}.{hashed}";
+        }
+
+        public bool VerifyPassword(string hash, string password)
+        {
+            var parts = hash.Split('.', 2);
+
+            if (parts.Length != 2)
+            {
+                return false;
+            }
+
+            var salt = Convert.FromBase64String(parts[0]);
+            var passwordHash = parts[1];
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return passwordHash == hashed;
         }
 
     }
+
+
+}
 
 }
