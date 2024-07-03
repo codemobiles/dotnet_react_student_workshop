ba9b54c1dfd3fd81f299464e683a1680af259c84
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-03 10:08:26 +0700

    1234

--- core_workshop/backend/Models/User.cs
+++ core_workshop/backend/Models/User.cs
@@ -5,13 +5,13 @@ namespace backend.Models;
 
 public partial class User
 {
-    public int Id { get; set; }
+    public int? Id { get; set; }
 
     public string Username { get; set; } = null!;
 
-    public string? Password { get; set; }
+    public string Password { get; set; } = null!;
 
     public string? Position { get; set; }
 
-    public DateTime Created { get; set; }
+    public DateTime? Created { get; set; }
 }
