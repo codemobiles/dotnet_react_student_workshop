24fe837337739334398b4d6e25aae1b7059fdfbc
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-03 14:53:31 +0700

    1234

--- core_workshop/frontend/src/store/slices/authSlice.ts
+++ core_workshop/frontend/src/store/slices/authSlice.ts
@@ -1,5 +1,8 @@
 import { LoginResult, RegisterResult } from "@/types/auth-result.type";
 import { createSlice } from "@reduxjs/toolkit";
+import { httpClient } from "@/utils/HttpClient";
+import { User } from "@/types/user.type";
+import { server } from "@/utils/constants";
 
 export interface AuthState {
   loginResult?: LoginResult;
@@ -13,6 +16,19 @@ const initialState: AuthState = {
   isAuthenticating: true,
   isAuthented: false,
   isError: false,
+};
+
+
+const login = async (value: User) => {
+  const result = await httpClient.post<LoginResult>(server.LOGIN_URL, value);
+
+  const { token } = result.data;
+  if (token) {
+    localStorage.setItem(server.TOKEN_KEY, token);
+    return result.data;
+  }
+
+  throw Error();
 };
 
 const authSlice = createSlice({
