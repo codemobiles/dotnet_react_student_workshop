b8116860e09a86d8af2e439831b48e443857d630
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-03 14:46:50 +0700

    1234

--- core_workshop/frontend/src/store/slices/stockSlice.ts
+++ core_workshop/frontend/src/store/slices/stockSlice.ts
@@ -1,68 +1,68 @@
-import { Product } from "@/types/product.type";
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { server } from "../../utils/constants";
-import { RootState } from "../store";
-import { httpClient } from "../../utils/HttpClient";
+// import { Product } from "@/types/product.type";
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import { server } from "../../utils/constants";
+// import { RootState } from "../store";
+// import { httpClient } from "../../utils/HttpClient";
 
-export interface StockState {
-  stockAllResult: Product[];
-  stockOneResult: Product | null;
-}
+// export interface StockState {
+//   stockAllResult: Product[];
+//   stockOneResult: Product | null;
+// }
 
-const initialState: StockState = {
-  stockAllResult: [],
-  stockOneResult: null,
-};
+// const initialState: StockState = {
+//   stockAllResult: [],
+//   stockOneResult: null,
+// };
 
-// Add
-export const addProduct = createAsyncThunk("stock/add", async (formData: FormData) => {
-  await httpClient.post(server.PRODUCT_URL, formData);
-});
+// // Add
+// export const addProduct = createAsyncThunk("stock/add", async (formData: FormData) => {
+//   await httpClient.post(server.PRODUCT_URL, formData);
+// });
 
-// Query
-export const getProducts = createAsyncThunk("stock/getAll", async (keyword: string): Promise<Product[]> => {
-  
-  if (keyword) {
-    const result = await httpClient.get<Product[]>(`${server.PRODUCT_URL}/search/name?keyword=${keyword}`);
-    return result.data;
-  } else {
-    const result = await httpClient.get<Product[]>(server.PRODUCT_URL);
-    return result.data;
-  }
-});
+// // Query
+// export const getProducts = createAsyncThunk("stock/getAll", async (keyword: string): Promise<Product[]> => {
 
-// Query by Id
-export const getProductById = createAsyncThunk("stock/getById", async (id: string): Promise<Product> => {
-  const result = await await httpClient.get(`${server.PRODUCT_URL}/${id}`);
-  return result.data;
-});
+//   if (keyword) {
+//     const result = await httpClient.get<Product[]>(`${server.PRODUCT_URL}/search/name?keyword=${keyword}`);
+//     return result.data;
+//   } else {
+//     const result = await httpClient.get<Product[]>(server.PRODUCT_URL);
+//     return result.data;
+//   }
+// });
 
-// Delete
-export const deleteProduct = createAsyncThunk("stock/delete", async (id: string) => {
-  await httpClient.delete(`${server.PRODUCT_URL}/${id}`);
-});
+// // Query by Id
+// export const getProductById = createAsyncThunk("stock/getById", async (id: string): Promise<Product> => {
+//   const result = await await httpClient.get(`${server.PRODUCT_URL}/${id}`);
+//   return result.data;
+// });
 
-// Edit
-export const editProduct = createAsyncThunk("stock/edit", async (formData: any) => {
-  await httpClient.put(server.PRODUCT_URL, formData);
-});
+// // Delete
+// export const deleteProduct = createAsyncThunk("stock/delete", async (id: string) => {
+//   await httpClient.delete(`${server.PRODUCT_URL}/${id}`);
+// });
 
-const stockSlice = createSlice({
-  name: "stock",
-  initialState: initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    // getProdcut
-    builder.addCase(getProducts.fulfilled, (state, action) => {
-      state.stockAllResult = action.payload;
-    });
+// // Edit
+// export const editProduct = createAsyncThunk("stock/edit", async (formData: any) => {
+//   await httpClient.put(server.PRODUCT_URL, formData);
+// });
 
-    // getProductById
-    builder.addCase(getProductById.fulfilled, (state, action) => {
-      state.stockOneResult = action.payload;
-    });
-  },
-});
+// const stockSlice = createSlice({
+//   name: "stock",
+//   initialState: initialState,
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     // getProdcut
+//     builder.addCase(getProducts.fulfilled, (state, action) => {
+//       state.stockAllResult = action.payload;
+//     });
 
-export const stockSelector = (store: RootState): StockState => store.stockReducer;
-export default stockSlice.reducer;
+//     // getProductById
+//     builder.addCase(getProductById.fulfilled, (state, action) => {
+//       state.stockOneResult = action.payload;
+//     });
+//   },
+// });
+
+// export const stockSelector = (store: RootState): StockState => store.stockReducer;
+// export default stockSlice.reducer;
