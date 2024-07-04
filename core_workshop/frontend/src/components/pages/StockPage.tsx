e56359f780d9405b7c247e3dbcdcf5af4cac2daa
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-04 10:35:39 +0700

    1234

--- core_workshop/frontend/src/components/pages/StockPage.tsx
+++ core_workshop/frontend/src/components/pages/StockPage.tsx
@@ -41,37 +41,3 @@ export default function DataTable() {
     </div>
   );
 }
-
-// import { getProducts, stockSelector } from "@/store/slices/stockSlice";
-// import { useAppDispatch } from "@/store/store";
-// import React, { useEffect } from "react";
-// import { useSelector } from "react-redux";
-
-// type Props = {};
-
-// export default function StockPage({}: Props) {
-//   const products = ["Product1", "Product2", "Product3", "Product4"];
-//   const stockReducer = useSelector(stockSelector);
-//   const dispatch = useAppDispatch();
-
-//   useEffect(() => {
-//     // first - called during created
-//     dispatch(getProducts());
-
-//     return () => {
-//       // second - called during destroyed
-//       console.log("Stock page was destroyed");
-//     };
-//   }, [dispatch]);
-
-//   return (
-//     <div>
-//       StockPage
-//       <ul>
-//         {stockReducer.stockAllResult.map((item) => (
-//           <li key={item._id}>{item.name}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
