0a482602e1f42185599845fbdb4af60f672505e7
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-04 16:17:25 +0700

    1234

--- core_workshop/frontend/src/components/pages/ReportPage.tsx
+++ core_workshop/frontend/src/components/pages/ReportPage.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-type Props = {}
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-export default function ReportPage({}: Props) {
-  return (
-    <div>ReportPage</div>
-  )
-}
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "My First dataset",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      backgroundColor: "rgba(75,192,192,0.2)",
+      borderColor: "rgba(75,192,192,1)",
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const LineChart: React.FC = () => {
+  return <Line data={data} options={options} />;
+};
+
+export default LineChart;
