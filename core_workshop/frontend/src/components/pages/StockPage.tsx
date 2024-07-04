77800a742655954424cf1c4626eaaa0f1a4b32cf
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-04 13:53:07 +0700

    1234

--- core_workshop/frontend/src/components/pages/StockPage.tsx
+++ core_workshop/frontend/src/components/pages/StockPage.tsx
@@ -108,6 +108,7 @@ const Stock = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
+  const [open, setOpen] = useState(false);
   const [openDialog, setOpenDialog] = useState<boolean>(false);
   const [selectedProduct, setSelectedProduct] = useState<Product>();
   const [value, setValue] = useDebounce("", 300);
@@ -238,6 +239,33 @@ const Stock = () => {
 
   const handleClose = () => {
     setOpenDialog(false);
+  };
+
+  const displayDlg = () => {
+    return (
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
   };
 
   const showDialog = () => {
