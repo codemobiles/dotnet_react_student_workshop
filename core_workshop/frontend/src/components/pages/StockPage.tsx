35b9b9029919e07567ee4d166f95b56f5f9facdd
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-04 11:20:29 +0700

    1234

--- core_workshop/frontend/src/components/pages/StockPage.tsx
+++ core_workshop/frontend/src/components/pages/StockPage.tsx
@@ -16,7 +16,7 @@ const columns: GridColDef[] = [
     width: 100,
     renderCell: ({ value }) => (
       <img
-        src="https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg"
+        src={"https://localhost:8081/images/" + value}
         alt=""
         className="h-[50px] w-[50px]"
       />
