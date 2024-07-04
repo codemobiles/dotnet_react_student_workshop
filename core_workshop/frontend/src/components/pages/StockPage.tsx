75ee1b890678998489cd5f2d2aa8d6907f290f67
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-04 10:33:25 +0700

    1234

--- core_workshop/frontend/src/components/pages/StockPage.tsx
+++ core_workshop/frontend/src/components/pages/StockPage.tsx
@@ -5,23 +5,9 @@ import { useAppDispatch } from "@/store/store";
 import { useSelector } from "react-redux";
 
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
-  },
+  { field: "productId", headerName: "ID", width: 70 },
+  { field: "name", headerName: "Name", width: 130 },
+  { field: "price", headerName: "Price", width: 130 },
 ];
 
 export default function DataTable() {
