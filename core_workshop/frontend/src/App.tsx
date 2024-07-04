5b59336db2740484e9f42ab95efcc1873f42e4ba
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-04 09:41:02 +0700

    1234

--- core_workshop/frontend/src/App.tsx
+++ core_workshop/frontend/src/App.tsx
@@ -41,6 +41,15 @@ const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
   }),
 }));
 
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
 export default function App() {
   const [open, setOpen] = React.useState(true);
   const authReducer = useSelector(authSelector);
@@ -70,6 +79,7 @@ export default function App() {
       )}
       <Main open={open}>
         <Container>
+          <DrawerHeader />
           <Routes>
             {/* public zone */}
             <Route
