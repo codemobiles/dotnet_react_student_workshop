5f960e8991245955ba7d3556d3fc24c2d869e1fd
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-04 10:28:31 +0700

    1234

--- core_workshop/frontend/src/components/pages/StockPage.tsx
+++ core_workshop/frontend/src/components/pages/StockPage.tsx
@@ -1,33 +1,86 @@
-import { getProducts, stockSelector } from "@/store/slices/stockSlice";
-import { useAppDispatch } from "@/store/store";
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import * as React from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
-type Props = {};
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 90,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
+  },
+];
 
-export default function StockPage({}: Props) {
-  const products = ["Product1", "Product2", "Product3", "Product4"];
-  const stockReducer = useSelector(stockSelector);
-  const dispatch = useAppDispatch();
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
 
-  useEffect(() => {
-    // first - called during created
-    dispatch(getProducts());
-
-    return () => {
-      // second - called during destroyed
-      console.log("Stock page was destroyed");
-    };
-  }, [dispatch]);
-
+export default function DataTable() {
   return (
-    <div>
-      StockPage
-      <ul>
-        {stockReducer.stockAllResult.map((item) => (
-          <li key={item._id}>{item.name}</li>
-        ))}
-      </ul>
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
     </div>
   );
 }
+
+// import { getProducts, stockSelector } from "@/store/slices/stockSlice";
+// import { useAppDispatch } from "@/store/store";
+// import React, { useEffect } from "react";
+// import { useSelector } from "react-redux";
+
+// type Props = {};
+
+// export default function StockPage({}: Props) {
+//   const products = ["Product1", "Product2", "Product3", "Product4"];
+//   const stockReducer = useSelector(stockSelector);
+//   const dispatch = useAppDispatch();
+
+//   useEffect(() => {
+//     // first - called during created
+//     dispatch(getProducts());
+
+//     return () => {
+//       // second - called during destroyed
+//       console.log("Stock page was destroyed");
+//     };
+//   }, [dispatch]);
+
+//   return (
+//     <div>
+//       StockPage
+//       <ul>
+//         {stockReducer.stockAllResult.map((item) => (
+//           <li key={item._id}>{item.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
