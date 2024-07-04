f9cb1931fa0f99f8a53d87529182cca498709db0
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-04 10:31:13 +0700

    1234

--- core_workshop/frontend/src/components/pages/StockPage.tsx
+++ core_workshop/frontend/src/components/pages/StockPage.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { stockSelector, getProducts } from "@/store/slices/stockSlice";
+import { useAppDispatch } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 70 },
@@ -21,23 +24,24 @@ const columns: GridColDef[] = [
   },
 ];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
-
 export default function DataTable() {
+  const stockReducer = useSelector(stockSelector);
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    // first - called during created
+    dispatch(getProducts());
+
+    return () => {
+      // second - called during destroyed
+      console.log("Stock page was destroyed");
+    };
+  }, [dispatch]);
+
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
-        rows={rows}
+        rows={stockReducer.stockAllResult}
         columns={columns}
         initialState={{
           pagination: {
