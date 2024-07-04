e5eb38a67792fad45cc441ebfd11e8051c2679fb
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-04 13:46:12 +0700

    1234

--- core_workshop/frontend/src/store/slices/stockSlice.ts
+++ core_workshop/frontend/src/store/slices/stockSlice.ts
@@ -1,9 +1,8 @@
 import { Product } from "@/types/product.type";
-import { httpClient } from "@/utils/HttpClient";
-import { server } from "@/utils/constants";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { act } from "react";
+import { server } from "../../utils/constants";
 import { RootState } from "../store";
+import { httpClient } from "../../utils/HttpClient";
 
 export interface StockState {
   stockAllResult: Product[];
@@ -15,26 +14,72 @@ const initialState: StockState = {
   stockOneResult: null,
 };
 
-export const getProducts = createAsyncThunk("stock/getProducts", async () => {
-  const result = await httpClient.get<Product[]>(server.PRODUCT_URL);
-  return result.data;
-});
+// Add
+export const addProduct = createAsyncThunk(
+  "stock/add",
+  async (formData: FormData) => {
+    await httpClient.post(server.PRODUCT_URL, formData);
+  }
+);
 
-// deleteProduct;
+// Query
+export const getProducts = createAsyncThunk(
+  "stock/getAll",
+  async (keyword: string): Promise<Product[]> => {
+    if (keyword) {
+      const result = await httpClient.get<Product[]>(
+        `${server.PRODUCT_URL}/search/name?keyword=${keyword}`
+      );
+      return result.data;
+    } else {
+      const result = await httpClient.get<Product[]>(server.PRODUCT_URL);
+      return result.data;
+    }
+  }
+);
+
+// Query by Id
+export const getProductById = createAsyncThunk(
+  "stock/getById",
+  async (id: string): Promise<Product> => {
+    const result = await await httpClient.get(`${server.PRODUCT_URL}/${id}`);
+    return result.data;
+  }
+);
+
+// Delete
 export const deleteProduct = createAsyncThunk(
-  "stock/deleteProduct",
-  async (id: string) => {}
+  "stock/delete",
+  async (id: string) => {
+    await httpClient.delete(`${server.PRODUCT_URL}/${id}`);
+  }
+);
+
+// Edit
+export const editProduct = createAsyncThunk(
+  "stock/edit",
+  async (formData: any) => {
+    await httpClient.put(server.PRODUCT_URL, formData);
+  }
 );
 
 const stockSlice = createSlice({
   name: "stock",
-  initialState,
+  initialState: initialState,
   reducers: {},
   extraReducers: (builder) => {
+    // getProdcut
     builder.addCase(getProducts.fulfilled, (state, action) => {
       state.stockAllResult = action.payload;
     });
+
+    // getProductById
+    builder.addCase(getProductById.fulfilled, (state, action) => {
+      state.stockOneResult = action.payload;
+    });
   },
 });
-export const stockSelector = (state: RootState) => state.stockReducer;
+
+export const stockSelector = (store: RootState): StockState =>
+  store.stockReducer;
 export default stockSlice.reducer;
