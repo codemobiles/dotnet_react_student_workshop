66dff84a586aa3e9860d3ce6bfdf3f29f6600281
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-05 09:41:07 +0700

    1234

--- core_workshop/frontend/src/components/fragments/Payment.tsx
+++ core_workshop/frontend/src/components/fragments/Payment.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  order: string;
+};
 
-export default function Payment({}: Props) {
-  return <div>Payment</div>;
+export default function Payment({ order }: Props) {
+  const orderArray = JSON.parse(order) as any[];
+
+  return (
+    <div>
+      Payment <br />
+      <ul>
+        <li>xx</li>
+        <li>xx</li>
+      </ul>
+    </div>
+  );
 }
