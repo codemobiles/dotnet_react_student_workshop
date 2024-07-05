b3f7d3d7ce8620cf6a3016a6de4bda8582c79c1c
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-05 09:12:15 +0700

    1234

--- core_workshop/frontend/src/store/store.ts
+++ core_workshop/frontend/src/store/store.ts
@@ -3,10 +3,11 @@ import { useDispatch } from "react-redux";
 import commonReducer from "./slices/commonSlice";
 import authReducer from "./slices/authSlice";
 import stockReducer from "./slices/stockSlice";
+import shopReducer from "./slices/shopSlice";
 
 // let store: any = undefined;
 const store = configureStore({
-  reducer: { commonReducer, authReducer, stockReducer },
+  reducer: { commonReducer, authReducer, stockReducer, shopReducer },
   devTools: true,
 });
 
