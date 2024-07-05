b6b14179484df096361c7d8ede44b42626aafe1c
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-07-05 09:49:52 +0700

    1234

--- core_workshop/frontend/src/components/fragments/Payment.tsx
+++ core_workshop/frontend/src/components/fragments/Payment.tsx
@@ -4,15 +4,27 @@ type Props = {
   order: string;
 };
 
+type OrderType = {
+  productId: number;
+  name: string;
+  image: string;
+  stock: number;
+  price: number;
+  created: Date;
+  qty: number;
+};
+
 export default function Payment({ order }: Props) {
-  const orderArray = JSON.parse(order) as any[];
+  const orderArray = JSON.parse(order) as OrderType[];
 
   return (
     <div>
       Payment <br />
       <ul>
         {orderArray.map((item) => (
-          <li>{item.name}</li>
+          <li>
+            {item.name}: {item.price}
+          </li>
         ))}
       </ul>
     </div>
